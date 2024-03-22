98ec49f6875eeda13c8e6a15c588fccd622838be
Ask Snbb <asksnbb@gmail.com>
2024-03-22 11:05:12 +0530

    commited By hitesh

--- resources/views/academicgallery.blade.php
+++ resources/views/academicgallery.blade.php
@@ -9,7 +9,7 @@
             <ul class="edu-breadcrumb">
                 <li class="breadcrumb-item"><a href="/">Home</a></li>
                 <li class="separator"><i class="icon-angle-right"></i></li>
-                <li class="breadcrumb-item active" aria-current="page">Academic Gallery</li>
+                <li class="breadcrumb-item active" aria-current="page">Academic Gallery </li>
             </ul>
         </div>
     </div>
